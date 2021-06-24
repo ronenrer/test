6e7c0b35f40c173eda5cbfa9f97015a0e37d1127
ronenrer <ronenrer@gmail.com>
2021-06-24 22:52:15 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Game Over")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
